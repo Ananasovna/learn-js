9dcff3653399e30ad42b31ea75abd15e2a57d6db
Ananasovna <a.n.ushkova@yandex.ru>
2022-06-10 21:54:33 +0200

    feat: add part of length-of-missing-array problem solving

--- Codewars/length-of-missing-array.js
+++ Codewars/length-of-missing-array.js
@@ -1,23 +1,27 @@
 
 function missingArrayLength(arr) {
+  let result;
   if (arr.length == 0 || arr.some(el => el === null)) {
     return 0;
   } else {
-    let result = arr.sort().reduce((prevLength, current) => {
-      console.log(prevLength, current);
-      if (current.length == 0 || current === null) {
-        return 0;
-      } else if((current.length - prevLength) > 1) {
-        console.log(сurrent.length - 1);
+    let newArr = arr.sort();
+    result = newArr.reduce((prevLength, current) => {
+      console.log("prevLenth:", prevLength, "current:", current);
+      if ((current.length - prevLength) > 1) {
+        console.log("current:", current.length);
+        return (current.length - 2);
       } else {
-        prevLength = current.length;
+        return current.length;
       }
-    });
+    }, 0);
     console.log(result);
   }
-  
+  return result;
 }
 
-let arr = [[], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]];
+let arr = [ [ 5, 2, 9 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]];
+console.log("sorted:", arr.sort((a, b) => a - b));
 
 console.log(missingArrayLength(arr));
+
+// написать ф-b. для сорта по длинне, типа (a.length > b.length)
