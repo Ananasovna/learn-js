68ef8918b1fbf8240186385790989c675e1e30b4
Ananasovna <a.n.ushkova@yandex.ru>
2022-06-13 17:45:00 +0200

    feat: add par of length-of-missing-array problem solving

--- Codewars/length-of-missing-array.js
+++ Codewars/length-of-missing-array.js
@@ -1,27 +1,21 @@
 
-function missingArrayLength(arr) {
-  let result;
-  if (arr.length == 0 || arr.some(el => el === null)) {
+function getLengthOfMissingArray(arr) {
+  let result = 0;
+
+  if ( arr == null || arr == [] ||  arr.some(el => el.length == 0 ) ) {
     return 0;
   } else {
-    let newArr = arr.sort();
-    result = newArr.reduce((prevLength, current) => {
-      console.log("prevLenth:", prevLength, "current:", current);
+    arr.sort((a, b) => a.length - b.length).reduce((prevLength, current) => {
       if ((current.length - prevLength) > 1) {
-        console.log("current:", current.length);
-        return (current.length - 2);
-      } else {
+        result = current.length - 1
+      };
         return current.length;
-      }
     }, 0);
-    console.log(result);
+    
   }
   return result;
 }
 
-let arr = [ [ 5, 2, 9 ], [ 4, 5, 1, 1 ], [ 1 ], [ 5, 6, 7, 8, 9 ]];
-console.log("sorted:", arr.sort((a, b) => a - b));
+let arr = [ [ null ] ];
 
-console.log(missingArrayLength(arr));
-
-// написать ф-b. для сорта по длинне, типа (a.length > b.length)
+console.log(getLengthOfMissingArray(arr));
