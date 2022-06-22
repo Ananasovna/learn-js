c8560f4260ca69d96f72228c98b72f165646b420
Ananasovna <a.n.ushkova@yandex.ru>
2022-06-22 18:35:06 +0200

    feat: add task 10-13 homework part 2 solving

--- JS-it-kamasutra/hw10-13.js
+++ JS-it-kamasutra/hw10-13.js
@@ -1,46 +1,117 @@
 let inputs = document.getElementsByClassName('input');
 inputs = [...inputs];
 
-const changeValue = function (newValue) {
-  inputs.forEach(item => item.value = newValue);
+// const changeValue = function (newValue) {
+//   inputs.forEach(item => item.value = newValue);
+// }
+
+// changeValue('Nancy');
+// changeValue('Ushkova');
+// changeValue('Nancy');
+
+// const setError = function(id) {
+//   let element = document.getElementById(id);
+//   element.className = 'error';
+// }
+
+// const getSumm = function (a, b) {
+//   return a + b;
+// }
+
+
+// // const sayHello = function() {
+// //   alert('My name');
+// // }
+
+// // setTimeout(sayHello, 1000);
+
+// const timeoutError = function() {
+//   setError('div');
+// }
+
+// setTimeout(timeoutError, 1000);
+
+// function hideElement(id) {
+//   console.log(id);
+//   document.getElementById(id).className = 'hidden';
+// }
+
+// hideElement('input2');
+
+// function hideElement(id) {
+//   document.getElementById(id).className = '';
+// }
+
+
+// // setInterval(() => console.log('sec'), 1);
+
+// function deleteEl(id) {
+//   let el = document.getElementById(id);
+//   if (el != null) {
+//     el.remove();
+//   }
+// }
+
+// deleteEl('input1');
+
+// function classAdd(id, className) {
+//   let el = document.getElementById(id);
+//   console.log(el);
+//   if (el != null) {
+//     el.className = className;
+//   }
+// }
+
+// classAdd('textarea', 'error');
+
+
+// function makeNarrow(el) {
+//   classAdd(el, 'width-0');
+// }
+
+// makeNarrow('textarea');
+
+// function showEl(el) {
+//   classAdd(el, '');
+// }
+
+// showEl('textarea');
+
+function deleteElByValue(arr, valueForDelete) {
+  arr.forEach(item => {
+    if (item.value === valueForDelete) {
+      item.remove();
+    }
+  });
 }
 
-changeValue('Nancy');
-changeValue('Ushkova');
-changeValue('Nancy');
+// deleteElByValue(inputs, '3');
 
-const setError = function(id) {
-  let element = document.getElementById(id);
-  element.className = 'error';
+// function deleteElByClass(arr, valueForDelete) {
+//   arr.forEach(item => {
+//     if (item.classList.contains(valueForDelete)) {
+//       item.remove();
+//     }
+//   });
+// }
+
+// deleteElByClass(inputs, 'hey');
+
+function hasClassHey(el) {
+  if (el.classList.contains('hey')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-const getSumm = function (a, b) {
-  return a + b;
+// let el = document.getElementById('input2');
+// console.log(hasClassHey(el));
+
+function removeElementIfConditionsTrue(elements, predicate) {
+  elements.forEach(item => {
+    if (predicate(item)) item.remove();
+  });
 }
 
-
-const sayHello = function() {
-  alert('My name');
-}
-
-setTimeout(sayHello, 1000);
-
-const timeoutError = function() {
-  setError('div');
-}
-
-setTimeout(timeoutError, 1000);
-
-function hideElement(id) {
-  console.log(id);
-  document.getElementById(id).className = 'hidden';
-}
-
-hideElement('input2');
-
-function hideElement(id) {
-  document.getElementById(id).className = '';
-}
-
-
-// setInterval(() => console.log('sec'), 1);
+removeElementIfConditionsTrue(inputs, hasClassHey);
