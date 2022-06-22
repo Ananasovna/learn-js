fd28c1911bbc018e5265fb1ca890a16be46d8b52
Ananasovna <a.n.ushkova@yandex.ru>
2022-06-22 14:53:31 +0200

    feat: add homework 10-13 task part 1

--- JS-it-kamasutra/hw10-13.js
+++ JS-it-kamasutra/hw10-13.js
@@ -8,3 +8,39 @@ const changeValue = function (newValue) {
 changeValue('Nancy');
 changeValue('Ushkova');
 changeValue('Nancy');
+
+const setError = function(id) {
+  let element = document.getElementById(id);
+  element.className = 'error';
+}
+
+const getSumm = function (a, b) {
+  return a + b;
+}
+
+
+const sayHello = function() {
+  alert('My name');
+}
+
+setTimeout(sayHello, 1000);
+
+const timeoutError = function() {
+  setError('div');
+}
+
+setTimeout(timeoutError, 1000);
+
+function hideElement(id) {
+  console.log(id);
+  document.getElementById(id).className = 'hidden';
+}
+
+hideElement('input2');
+
+function hideElement(id) {
+  document.getElementById(id).className = '';
+}
+
+
+// setInterval(() => console.log('sec'), 1);
